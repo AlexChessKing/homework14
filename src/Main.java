dfdf816c6fb75770a533c36f3e8c67102c1fced3
Alexandr Yakovlev <engelsovich75@yandex.ru>
2022-10-06 11:31:00 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {4, 56, 0, 223, 500, 22};
+        SalesManager manager = new SalesManager(sales);
+        System.out.println("Макс: " + manager.max());
+    }
+}
