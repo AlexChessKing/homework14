f068e01e1df8f6983fbc1e71a52241762f808620
Alexandr Yakovlev <engelsovich75@yandex.ru>
2022-10-06 16:28:53 +0500

    adding empty lines

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int[] sales = {4, 56, 0, 223, 500, 22};
+
         SalesManager manager = new SalesManager(sales);
+
         System.out.println("Макс: " + manager.max());
     }
 }
